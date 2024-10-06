6efe54b77da11fa9b3f45ad4545a30111ec85519
Shivam Rajput <98.shivrajput@gmail.com>
2024-10-06 13:08:01 +0530

    Update MLstreamlit.py

--- MLstreamlit.py
+++ MLstreamlit.py
@@ -8,7 +8,7 @@ warnings.filterwarnings('ignore')
 
 def currenc(currency, grouping=True):
 
-    locale.setlocale(locale.LC_ALL, '')
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     return locale.currency(currency, symbol=True, grouping= grouping)
 
 
